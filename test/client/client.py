576cf6ac4fa4189a7d3db20f5009d1a0c18ce869
Yethal <nosuchemail@email.com>
2022-08-01 23:48:00 +0200

    redundant files

--- test/client/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-HOST = socket.gethostbyname('server')
-PORT = 12345
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"hello world")
